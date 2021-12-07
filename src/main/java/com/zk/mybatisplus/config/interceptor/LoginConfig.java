d0e07ba718b127c55e2bf084dce3300ea319b116
zhangkun <kun.zhang@dtarray.com>
2021-12-07 16:00:51 +0800

    增加腾讯云ocr识别

--- src/main/java/com/zk/mybatisplus/config/interceptor/LoginConfig.java
+++ src/main/java/com/zk/mybatisplus/config/interceptor/LoginConfig.java
@@ -1,58 +1,59 @@
-package com.zk.mybatisplus.config.interceptor;
-
-import com.zk.mybatisplus.mapper.TTenantUserMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import javax.annotation.Resource;
-
-
-/**
- * 拦截器配置
- */
-@Configuration
-public class LoginConfig implements WebMvcConfigurer {
-
-    @Resource
-    private TTenantUserMapper userMapper;
-
-    /**
-     *  配置拦截器、拦截路径
-     *  每次请求到拦截的路径，就会去执行拦截器中的方法
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //注册自己写的拦截器，如果是多个拦截器，形成一个拦截器链
-        InterceptorRegistration registration = registry.addInterceptor(new AuthInterceptor(userMapper));
-        registration.addPathPatterns("/**");             //所有路径被拦截
-        registration.excludePathPatterns(//添加不拦截路径
-                "/swagger-resources",//swagger路径
-                "/root/login",   //登陆路径
-                "/root/auth",    //注册路径
-                "/**/*.html",    //html静态资源
-                "/**/*.js",      //js静态资源
-                "/**/*.css");    //css静态资源
-    }
-
-
-    /**
-     * 解决跨域请求
-     */
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true);
-    }
-
-    //注入拦截器
-    @Bean
-    public AuthInterceptor AuthInterceptor() {
-        return new AuthInterceptor(userMapper);
-    }
-
-}
+//package com.zk.mybatisplus.config.interceptor;
+//
+//import com.zk.mybatisplus.mapper.TTenantUserMapper;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import javax.annotation.Resource;
+//
+//
+///**
+// * 拦截器配置
+// */
+//@Configuration
+//public class LoginConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    private TTenantUserMapper userMapper;
+//
+//    /**
+//     *  配置拦截器、拦截路径
+//     *  每次请求到拦截的路径，就会去执行拦截器中的方法
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        //注册自己写的拦截器，如果是多个拦截器，形成一个拦截器链
+//        InterceptorRegistration registration = registry.addInterceptor(new AuthInterceptor(userMapper));
+//        registration.addPathPatterns("/**");             //所有路径被拦截
+//        registration.excludePathPatterns(//添加不拦截路径
+//                "/swagger-resources",//swagger路径
+//                "/root/login",   //登陆路径
+//                "/root/auth",    //注册路径
+//                "/**/*.html",    //html静态资源
+//                "/**/*.js",      //js静态资源
+//                "/ocr/index",      //ocr
+//                "/**/*.css");    //css静态资源
+//    }
+//
+//
+//    /**
+//     * 解决跨域请求
+//     */
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .allowedOriginPatterns("*")
+//                .allowCredentials(true);
+//    }
+//
+//    //注入拦截器
+//    @Bean
+//    public AuthInterceptor AuthInterceptor() {
+//        return new AuthInterceptor(userMapper);
+//    }
+//
+//}
---
+++ src/main/java/com/zk/mybatisplus/controller/OcrController.java
@@ -0,0 +1,105 @@
+package com.zk.mybatisplus.controller;
+
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.ocr.v20181119.OcrClient;
+import com.tencentcloudapi.ocr.v20181119.models.IDCardOCRRequest;
+import com.tencentcloudapi.ocr.v20181119.models.IDCardOCRResponse;
+import com.tencentcloudapi.tia.v20180226.models.Model;
+import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.entity.ContentType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/ocr")
+public class OcrController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "ocr";
+    }
+
+    @PostMapping("uploadFile")
+    @ResponseBody
+    public IDCardOCRResponse OCRIdCardTest(@RequestParam(value = "file") MultipartFile file, @RequestParam(value = "card_side") String cardSize, Model model){
+        try {
+            Credential cred = new Credential("AKIDf9OcgVzOVO2Rq2jA8pLdfQyGKLH0Ohld", "qSq9F6i780cyHxOZkiRzNjrkofHdlodI");
+
+            HttpProfile httpProfile = new HttpProfile();
+            httpProfile.setEndpoint("ocr.tencentcloudapi.com");
+
+            ClientProfile clientProfile = new ClientProfile();
+            clientProfile.setHttpProfile(httpProfile);
+
+            OcrClient client = new OcrClient(cred, "ap-beijing", clientProfile);
+            Map<String, String> params = new HashMap<>();
+           // params.put("ImageBase64", getBase64FromInputStream(file.getInputStream()));
+            params.put("ImageBase64", file2Base64(file.getInputStream()));
+            params.put("CardSide", cardSize);
+            System.out.println(getBase64FromInputStream(file.getInputStream()));
+
+            IDCardOCRRequest req = IDCardOCRRequest.fromJsonString(JSONObject.fromObject(params).toString(), IDCardOCRRequest.class);
+            IDCardOCRResponse resp = client.IDCardOCR(req);
+            return resp;
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public static String file2Base64(InputStream in ) {
+        String base64 = null;
+        try {
+            byte[] buff = new byte[in.available()];
+            in.read(buff);
+            base64 = new String(Base64.encodeBase64(buff));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return base64;
+    }
+    public static String getBase64FromInputStream(InputStream in) {
+        // 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        byte[] data = null;
+        // 读取图片字节数组
+        try {
+            ByteArrayOutputStream swapStream = new ByteArrayOutputStream();
+            byte[] buff = new byte[100];
+            int rc = 0;
+            while ((rc = in.read(buff, 0, 100)) > 0) {
+                swapStream.write(buff, 0, rc);
+            }
+            data = swapStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return new String(Base64.encodeBase64(data));
+    }
+}
